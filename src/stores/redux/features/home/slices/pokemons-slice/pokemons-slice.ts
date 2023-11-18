0de2a0b5b411bc02db0cadaa4237eeba4ac9e2f9
VadimToma <https://github.com/TomaVadim>
2023-11-18 17:09:19 +0200

    chore: update root reducer with pokemon slice

--- src/stores/redux/features/home/slices/pokemons-slice/pokemons-slice.ts
+++ src/stores/redux/features/home/slices/pokemons-slice/pokemons-slice.ts
@@ -1,6 +1,7 @@
+import { PokemonData } from "@/types/pokemon-data";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState: PokemonData[] = [];
 
 export const pokemonsSlice = createSlice({
   name: "pokemons",
--- src/stores/redux/root-reducer/root-reducer.ts
+++ src/stores/redux/root-reducer/root-reducer.ts
@@ -1,5 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-export const rootReducer = combineReducers();
+import pokemonsSlice from "@/stores/redux/features/home/slices/pokemons-slice/pokemons-slice";
+
+export const rootReducer = combineReducers({
+  pokemons: pokemonsSlice,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
