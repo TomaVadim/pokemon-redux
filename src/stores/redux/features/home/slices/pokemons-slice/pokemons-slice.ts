4cf0180ec55bd689361193b7dc00510f032da5a7
VadimToma <https://github.com/TomaVadim>
2023-11-18 16:56:56 +0200

    feat: create pokemons slice

---
+++ src/stores/redux/features/home/slices/pokemons-slice/pokemons-slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const pokemonsSlice = createSlice({
+  name: "pokemons",
+  initialState,
+  reducers: {},
+});
+
+export default pokemonsSlice.reducer;
